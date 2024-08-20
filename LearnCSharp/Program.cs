dda399d85a44db4faed4b46ba5ab94511e29be4b
Lugiyan <46066547+prescalamity@users.noreply.github.com>
2024-08-20 12:20:57 +0800

    初始化工程增加忽略文件

---
+++ LearnCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LearnCSharp;
+
+Console.WriteLine("Hello, World! by CSharp_DotNet_Narwhal");
+
+MainTest mainTest = new MainTest();
+
+mainTest.RunThis();
+
+
+
+
